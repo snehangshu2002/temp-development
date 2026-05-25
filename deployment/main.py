4bd6e539a98b6d6743ff700ec53a46012de5f31f
snehangshu2002 <snehangshubhuin@gmail.com>
2026-05-25 21:47:30 +0530

    Add routes for home, reviews, and contact pages with template rendering

--- deployment/main.py
+++ deployment/main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 # initializing the app
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/reviews")
+def review():
+    return "Welcome to Your Review Page"
+
+@app.route("/contact")
+def contact():
+    return "Welcome to contact Page"
 # run the app
 app.run()
+
