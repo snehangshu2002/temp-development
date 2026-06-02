ca2a6dc02e190aa70eab65ee101396f497dd3121
snehangshu2002 <snehangshubhuin@gmail.com>
2026-06-02 21:17:02 +0530

    fix(app): correct formatting of main execution block

--- http methods/app.py
+++ http methods/app.py
@@ -154,5 +154,5 @@ def get_student(student_id):
     return succes_response(message=f"Student fetched successgully", data=student)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
